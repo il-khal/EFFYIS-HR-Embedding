a4305f4aaa38af5969ac4d67f66d44e851b5f8f0
Ilyas Khallouki <khalloukielias@gmail.com>
2024-08-13 15:14:20 +0100

    Changed return structure and general improvments

--- rp_handler.py
+++ rp_handler.py
@@ -1,19 +1,18 @@
 import runpod
 from tokenizer import tokenize, embed_text, embed_sentences, embed_words
-
+from fastapi.encoders import jsonable_encoder
 
 def process_input(input):
     text = input['text']
     full_text, sentences, words = tokenize(text)
-    embedded_full_text = embed_text(full_text)
+    # embedded_full_text = embed_text(full_text)
     embedded_sentences = embed_sentences(sentences)
     embedded_words = embed_words(words)
 
-    return {
-        "full text" : embedded_full_text,
+    return jsonable_encoder({
         "sentences" : embedded_sentences, 
-        "words" : embedded_words,
-    }
+        "words" : embedded_words
+    })
 
 def handler(event):
     return process_input(event['input'])
