ea3a7574d9add667abc977064366a044604519ff
Ilyas Khallouki <khalloukielias@gmail.com>
2024-08-27 11:55:19 +0100

    Updated the handler

--- rp_handler.py
+++ rp_handler.py
@@ -1,17 +1,15 @@
 import runpod
-from tokenizer import tokenize, embed_text, embed_sentences, embed_words
+from tokenizer import process_text
+from model import get_embeddings
 from fastapi.encoders import jsonable_encoder
 
 def process_input(input):
     text = input['text']
-    full_text, sentences, words = tokenize(text)
-    # embedded_full_text = embed_text(full_text)
-    # embedded_sentences = embed_sentences(sentences)
-    embedded_words = embed_words(words)
+    processed_text = process_text(text)
+    embeddings = get_embeddings(processed_text)
 
     return jsonable_encoder({
-        # "sentences" : embedded_sentences, 
-        "words" : embedded_words
+        embeddings: embeddings
     })
 
 def handler(event):
